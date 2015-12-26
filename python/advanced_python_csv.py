ef135172242950af98be634980e47bc0897033f4
RuiChang123 <changrui.c@gmail.com>
2015-12-26 08:55:04 -0800

    advanced_python_csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,19 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+
+#read file
+path='/Users/ruichang/ds/metis/prework/dsp/python/faculty.csv'
+f=pd.read_csv(path)
+f.columns=['name','degree','title','email'] #set column names
+
+emails=[x for x in f['email']]
+#add '/n' at the end of each email address
+email_list=[]
+for e in emails:
+    email_list=email_list+[e]+['\n']
+#creat file
+path2='/Users/ruichang/ds/metis/prework/dsp/python/emails.csv'
+f=open(path2,'w')
+#write email list to the file
+f.writelines(email_list)
+f.close()
+
