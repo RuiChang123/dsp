fed22fdd38b8be2e8477b13bf0ea98eed6a2d31c
RuiChang123 <changrui.c@gmail.com>
2015-12-26 08:57:39 -0800

    advanced_python_dict

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+#read file
+path='/Users/ruichang/ds/metis/prework/dsp/python/faculty.csv'
+f=pd.read_csv(path)
+f.columns=['name','degree','title','email'] #set column names
+#change all the phd formates in the dataframe to "Ph.D."
+for i in range(len(f['degree'])):
+    x=f['degree'][i].strip(' ').split(' ')
+    for e in range(len(x)):
+        if 'Ph' in x[e]:
+            x[e]='Ph.D.'
+    f['degree'][i]=' '.join(x)
+#change full names to last names
+for i in range(len(f['name'])):
+    x=f['name'][i].split(' ')
+    f['name'][i]=x[-1]
